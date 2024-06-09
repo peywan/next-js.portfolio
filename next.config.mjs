ecfe55288f4a8d9d97ecc327d95258177c4c3dc7
Peywan Sabir <peywan1999@icloud.com>
2024-06-09 12:43:52 +0200

    fixed issues now running

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+    reactStrictMode: true,
+    images: {
+        domains: ['scontent.farn2-2.fna.fbcdn.net'],
+    },
+};
