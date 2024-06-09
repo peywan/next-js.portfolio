1738f56f46c6c944233b1692d073fc8c92df4578
Peywan Sabir <peywan1999@icloud.com>
2024-06-09 11:15:38 +0200

    fixed deploy issues for vercel

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,5 @@
 "use client";
+import Image from 'next/image';
 
 const AboutMe = () => {
     return (
@@ -6,7 +7,7 @@ const AboutMe = () => {
             <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md p-6 mb-4 mx-auto max-w-3xl">
                 <h2 className="text-3xl font-bold mb-4">About Me</h2>
                 <p className="text-lg text-gray-700 dark:text-gray-300">
-                    Hello! I'm Peywan, a frontend developer from Stockholm, Sweden. I have a passion for coding and enjoy creating beautiful and functional web applications. With a background in martial arts, I bring discipline and focus to my work, striving for excellence in every project. I'm always eager to learn new technologies and improve my skills.
+                    Hello! I&apos;m Peywan, a frontend developer from Stockholm, Sweden. I have a passion for coding and enjoy creating beautiful and functional web applications. With a background in martial arts, I bring discipline and focus to my work, striving for excellence in every project. I&apos;m always eager to learn new technologies and improve my skills.
                 </p>
             </div>
         </section>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,5 @@
 "use client";
+import Image from 'next/image';
 import { useTheme } from '../context/ThemeContext';
 
 const HeroSection = () => {
@@ -7,16 +8,18 @@ const HeroSection = () => {
     return (
         <section className={`font-size-${state.userPreferences.fontSize} py-6`}>
             <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md p-6 mb-4 mx-auto max-w-3xl">
-                <img
-                    src="https://scontent.farn2-2.fna.fbcdn.net/v/t39.30808-6/424928883_783655863790790_8510291933938260959_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=WYJ8vyx2uh0Q7kNvgHo2365&_nc_ht=scontent.farn2-2.fna&oh=00_AYBhVeK46HF8RxfxVOUeTnEu6PiYKTTcCi-e3ehJaJpSnQ&oe=66694529"
+                <Image
+                    src={"https://scontent.farn2-2.fna.fbcdn.net/v/t39.30808-6/424928883_783655863790790_8510291933938260959_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=WYJ8vyx2uh0Q7kNvgHo2365&_nc_ht=scontent.farn2-2.fna&oh=00_AYBhVeK46HF8RxfxVOUeTnEu6PiYKTTcCi-e3ehJaJpSnQ&oe=66694529"}
                     alt="Profile"
+                    width={128} // Define the width
+                    height={128} // Define the height
                     className="rounded-full w-32 h-32 mx-auto mb-4"
                 />
                 <h1 className={`text-4xl font-bold mb-4 ${state.theme === 'light' ? 'text-gray-900' : 'text-white'}`}>
                     Welcome to My Portfolio
                 </h1>
                 <p className={`text-xl ${state.theme === 'light' ? 'text-gray-700' : 'text-gray-300'}`}>
-                    I'm a frontend developer with a passion for creating interactive and dynamic user experiences.
+                    I&apos;m a frontend developer with a passion for creating interactive and dynamic user experiences.
                 </p>
             </div>
         </section>
